464d304fb81c3e66fcdc3c1b27a3b4e2f77210d1
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-07-20 19:11:54 +0500

    Compose multipart messages

--- Mailer.php
+++ Mailer.php
@@ -14,7 +14,80 @@ declare(strict_types=1);
 
 namespace Comely\IO\Mailer;
 
+use Comely\IO\Mailer\Agents\EmailAgentInterface;
+use Comely\IO\Mailer\Agents\Sendmail;
+use Comely\IO\Mailer\Messages\Message;
+use Comely\IO\Mailer\Messages\Sender;
+
+/**
+ * Class Mailer
+ * @package Comely\IO\Mailer
+ */
 class Mailer
 {
+    /** @var EmailAgentInterface */
+    private $agent;
+    /** @var Sender */
+    private $defaultSender;
+    /** @var string */
+    private $eol;
 
+    /**
+     * Mailer constructor.
+     */
+    public function __construct()
+    {
+        $this->defaultSender = new Sender();
+        $this->agent = new Sendmail();
+        $this->eol = "\r\n";
+    }
+
+    /**
+     * @param null|string $eol
+     * @return string
+     */
+    public function eol(?string $eol = null): string
+    {
+        if ($eol) {
+            $this->eol = $eol;
+        }
+
+        return $this->eol;
+    }
+
+    /**
+     * @return Sender
+     */
+    public function sender(): Sender
+    {
+        return $this->defaultSender;
+    }
+
+    /**
+     * @param EmailAgentInterface $agent
+     * @return Mailer
+     */
+    public function agent(EmailAgentInterface $agent): self
+    {
+        $this->agent = $agent;
+        return $this;
+    }
+
+    /**
+     * @return Message
+     */
+    public function compose(): Message
+    {
+        return new Message($this);
+    }
+
+    /**
+     * @param Message $message
+     * @param string ...$emails
+     * @return int
+     */
+    public function send(Message $message, string ...$emails): int
+    {
+        return $this->agent->send($message, $emails);
+    }
 }
